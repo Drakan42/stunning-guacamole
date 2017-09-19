e6d7e54976a7c2854101da5c37e6d052a3f4fbe6
Daniel Alarcao <daniel.alarcao@lunatech.com>
2017-09-19 11:10:10 +0200

    added a test job

---
+++ testjob.groovy
@@ -0,0 +1,11 @@
+job('DSL-Tutorial-1-Test') {
+    scm {
+        git('git://github.com:Drakan42/Airport-demo.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
