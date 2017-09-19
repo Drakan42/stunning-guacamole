3db60c704b27a2a5b233b82592d5afa631983460
Daniel Alarcao <daniel.alarcao@lunatech.com>
2017-09-19 12:09:17 +0200

    test

--- testjob.groovy
+++ testjob.groovy
@@ -1,6 +1,6 @@
 job('Test') {
     scm {
-        git('git://github.com:Drakan42/Airport-demo.git')
+        git('git:/github.com:Drakan42/Airport-demo.git')
     }
     triggers {
         scm('H/15 * * * *')
