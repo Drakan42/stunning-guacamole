9bdb4d6bb2242097fb8f44a2d8d040f92ae429ae
Daniel Alarcao <daniel.alarcao@lunatech.com>
2017-09-20 16:16:48 +0200

    assssssssss

--- testjob.groovy
+++ testjob.groovy
@@ -2,7 +2,7 @@ pipelineJob('Test') {
   definition{
     cpsScm {
         scm {
-            git('https://github.com/Drakan42/stunning-guacamole.git','docker')
+            git('https://github.com/Drakan42/Airport-demo.git','docker')
         }
     }
   }
